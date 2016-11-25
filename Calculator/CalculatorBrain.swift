cfebdf5cb0f0025205eaae982a798b4a60696a25
William E. Seyler <bill@billandlizz.com>
2016-11-24 20:48:44 -0500

    Updated for programmable calculator

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,9 +11,10 @@ import Foundation
 class CalculatorBrain {
     
     private var accumulator = 0.0
-    
+    private var internalProgram = [AnyObject]()
     func setOperand(operand: Double) {
         accumulator = operand
+        internalProgram.append(operand as AnyObject)
     }
     
     private var operations: Dictionary<String,Operation> = [
@@ -37,6 +38,7 @@ class CalculatorBrain {
     }
     
     func performOperation(symbol: String) {
+        internalProgram.append(symbol as AnyObject)
         if let operation = operations[symbol] {
             switch operation {
             case .Constant(let value):
@@ -64,6 +66,32 @@ class CalculatorBrain {
     private struct PendingBinaryOperationInfo {
         var binaryFunction: (Double, Double) -> Double
         var firstOperand: Double
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram as CalculatorBrain.PropertyList
+        }
+        set {
+            clear()
+            if let arrayOfOpts = newValue as? [AnyObject] {
+                for op in arrayOfOpts {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
     }
     
     var result: Double {
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -34,6 +34,19 @@ class ViewController: UIViewController {
         }
     }
     
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
     private var brain = CalculatorBrain()
     
     @IBAction private func performOperation(_ sender: UIButton) {
